e00e105f228ea2b109f532607b446dd4c2fe42ba
Miguel Ángel <MAB015@HOTMAIL.COM>
2023-03-18 02:02:55 -0500

    EN - Creating a State of Images changing the generic type to a Array string ES - Creando un estado de imagenes cambiando el tipo generico a un array de string
    EN - Adding types to the React hook
    ES - Agregando tips al hook de react
    
    EN - We cycled through images placing our component with the corresponding image
    ES - Ciclamos a traves de imagenes colocando nuestro componente con la imagen a la que corresponde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client"; // this is a client component 👈🏽
+
+import { useState } from 'react'
 import { Inter } from 'next/font/google'
 import { RandomFox  } from './components/RandomFox'
 
@@ -7,15 +10,30 @@ const inter = Inter({ subsets: ['latin'] })
 const random = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+  // Creando un estado de imagenes cambiando el tipo generico a un array de string
+  // Agregando tips al hook de react
+  const [images, setImages] = useState<string[]>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`
+  ])
+
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
 
-      
-      {/* Crea URL de imagen */}
-      <RandomFox image={ `https://randomfox.ca/images/${random()}.jpg`} />
+      {/* Ciclamos a travez de imagenes colocando nuestro componente con la imagen a la que corresponde */}
+      { images.map((image, index) => (
+        <div key={index} className="p-4">
+          {/* Crea URL de imagen */}
+          <RandomFox image={image} />
+        </div>
+      )) }
     </main>
   )
 }
