59d3eb9bca1d07f38e03547c81e9b0894e73029c
Miguel Ángel <MAB015@HOTMAIL.COM>
2023-03-18 21:53:33 -0500

    EN - I created a button to add an event taht will generate a image of a new fox every time.
    EN - To receive the event I add to the const "newFoxImage" the type of "MouseEventHandler" to receive the right types and don't receive "any"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"; // this is a client component 👈🏽
 
 import { useState } from 'react'
+import { MouseEventHandler } from 'react'
 import { Inter } from 'next/font/google'
 import { RandomFox  } from './components/RandomFox'
 
@@ -9,6 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 // Generate a random function
 const random = () => Math.floor(Math.random() * 123) + 1
 
+// Creating the ImageItem Type
 type ImageItem = {id: string; url: string}
 
 // Generate Unique Id's
@@ -17,20 +19,34 @@ const generateId = () => Math.random().toString(36).substring(2,9)
 export default function Home() {
   // Creando un estado de imagenes cambiando el tipo generico a un array de string
   // Agregando tips al hook de react
-  const [images, setImages] = useState<Array<ImageItem>>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-  ])
+  const [images, setImages] = useState<Array<ImageItem>>([])
+
+  // Adding the type MouseEventHandler to have the exact type of the events
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault()
+    const target = event.target
+
+    // Generating the Image
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`,
+    }
+
+    // Changing the status of the Image
+    setImages([
+      newImageItem
+    ])
+  }
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+
+      {/* // Button to change a fox everytime */}
+      <button onClick={addNewFox}>Add new Fox</button>
+
 
       {/* Ciclamos a travez de imagenes colocando nuestro componente con la imagen a la que corresponde */}
       { images.map(({id, url}) => (
